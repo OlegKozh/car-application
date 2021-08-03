175f6f21af750d97bb948ee1fa4b1eaa7141e7ed
northlanekek <skater7878@gmail.com>
2021-08-03 22:26:27 +0300

    Removed unnecessary rows

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -16,11 +16,6 @@ public class App {
         // После приобретения автомобиля необходимо
         // Car car = carDealer.getCar(price, year, mileage);
         Car car = new Car(engine, wheels, lamps);
-        System.out.println(car.getDescription());
-        System.out.println(car.getDescription());
-        System.out.println(car.getDescription());
-        System.out.println(car.getDescription());
-        Car car2 = new Car(engine, wheels, lamps);
         List<Lamp> snatchedLamps = car.snatchLamps();
         for (Lamp lamp: snatchedLamps) {
             if (lamp.isDirty()) {
